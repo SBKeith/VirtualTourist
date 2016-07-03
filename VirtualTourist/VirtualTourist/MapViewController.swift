cca7d317904bbcfa071f88e92883c9a8c405f806
Keith Kowalski <keith@agilerocket.com>
2016-07-02 18:02:25 -0700

    Initial Setup

---
+++ VirtualTourist/VirtualTourist/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  VirtualTourist
+//
+//  Created by Keith Kowalski on 7/2/16.
+//  Copyright © 2016 TouchTapApp. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
